305e69259631c3c65d732810400fec8356842521
grada92 <danielegradassai@gmail.com>
2022-08-25 14:32:11 +0200

    correggo condizione

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ console.log('js ok');
 for(let number = 1; number <= 100; number++) {
 
     if(!isNaN(number)) {
-        const divisibilePerDue = number % 2 === 0;
-        const divisibilePerTre = number % 3 === 0;
-        const divisibileperDueeTre = number % 2 && number % 3 === 0;
+        const divisibilePerDue = number % 3 === 0;
+        const divisibilePerTre = number % 5 === 0;
+        const divisibileperDueeTre = number % 3 && number % 5 === 0;
 
 
         if(divisibilePerDue) {
