4a393bf07f6b5f78cf02296659ae29b3776107f0
grada92 <danielegradassai@gmail.com>
2022-08-25 15:22:26 +0200

    imposto condizione nondivisibile

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 console.log('js ok');
 
-for(let number = 1; number <= 100; number++) {
+for(let number = 1; number <= 100; number++) 
     // Imposto la condizione per cui il numero è divisibile
-
     if(!isNaN(number)) {
-        const divisibilePerTre = number % 3 === 0;
-        const divisibilePerCinque = number % 5 === 0;
-        const divisibilePerTreePerCinque = number % 3 && number % 5 === 0;
-
+        const nonDivisibile = number % 3 !== 0 && number % 5 != 0;
+        const divisibilePerTre = number % 3 === 0 && number % 5 != 0;
+        const divisibilePerCinque = number % 5 === 0 && number % 3 != 0;
+        const divisibilePerTreePerCinque = number % 3 === 0 && number % 5 === 0;
 
         if(divisibilePerTre) {
-            console.log(number +'fizz')
+            console.log(number + 'fizz')
         }
 
         if(divisibilePerCinque) {
@@ -20,7 +19,8 @@ for(let number = 1; number <= 100; number++) {
         if(divisibilePerTreePerCinque) {
             console.log(number + 'fizzbuzz')
         }
-    }
-        
 
-}
+        if(nonDivisibile) {
+            console.log(number)
+        }  
+    }
